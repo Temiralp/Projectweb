4d6bf76eaf1ee96787465fda3847579df391555a
ferhanserin <mail.ferhanserin@gmail.com>
2023-12-03 14:19:58 +0300

    ui modifiye edildi

--- ui.cs
+++ ui.cs
@@ -1,8 +1,8 @@
 public class UI {
 
     public void Button2(int d,int s);
-    public void Attack(int a) { }
-    public void Damage(int k) { }
+    public void Attacke(int a) { }
+    public void Damagee(int k) { }
 
     public void Attack(int f)
     {
