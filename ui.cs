76e351ac19ae9765979e8ce3259e8e84b1edf3a1
Esmanurcalis <esmanurcalis4@gmail.com>
2023-12-03 14:26:20 +0300

    duzenlendı

--- ui.cs
+++ ui.cs
@@ -3,7 +3,7 @@ public class UI {
 
 
     public void Button2(int d,int s);
-    public void Attackef(int a) { }
+    public void Attackef(int a,int e) { }
     public void Damagee(int k) { }
 
     public void Button2(string b = "ilayda");
